2a084eeeb31fe5d87de6eac63712dbd02e5725aa
Sifcoon <abedul00@gmail.com>
2020-04-10 14:31:02 -0500

    Disposisiones Cofepris
    se agrega archivo PDF de las disposiciones de Cofepris

---
+++ Ventilador.mm
@@ -0,0 +1,183 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Ventilador" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1211756865" CREATED="1586541323287" MODIFIED="1586541606777"><hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork">
+<font NAME="Consolas" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details">
+<font NAME="Consolas"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font NAME="Consolas" SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT">
+<font NAME="Consolas"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+<font NAME="Consolas"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="fork" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font NAME="Consolas" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#003399">
+<font NAME="Consolas" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#0066ff">
+<font NAME="Consolas" SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#666666">
+<font NAME="Consolas" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font NAME="Consolas" SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<font NAME="Consolas"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<font NAME="Consolas"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<font NAME="Consolas"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<font NAME="Consolas"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<font NAME="Consolas"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<font NAME="Consolas"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<font NAME="Consolas"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<node TEXT="COFEPRIS" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="right" ID="ID_477507455" CREATED="1586541325797" MODIFIED="1586541667740"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Requerimientos
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="Modos de ventilaci&#xf3;n" ID="ID_168846931" CREATED="1586541669010" MODIFIED="1586541696972"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Al menos uno de los dos modos
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="Ventilacion controlada" ID="ID_768420987" CREATED="1586541698361" MODIFIED="1586541715193">
+<node TEXT="Para pacientes sedados" ID="ID_15598176" CREATED="1586541742074" MODIFIED="1586541748595"/>
+<node TEXT="Establecer" ID="ID_1589944037" CREATED="1586541748994" MODIFIED="1586542133884">
+<node TEXT="" ID="ID_353195100" CREATED="1586542170826" MODIFIED="1586542170826">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="volumen de corriente" ID="ID_1073897020" CREATED="1586542133884" MODIFIED="1586542139594"/>
+<node TEXT="salida de flujo regulado por presion para lograr el volumen" ID="ID_355436882" CREATED="1586542139720" MODIFIED="1586542160075"/>
+<node TEXT="" ID="ID_887313870" CREATED="1586542170826" MODIFIED="1586542170826">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="control de volumen regulado por presion" ID="ID_1481333463" CREATED="1586542170826" MODIFIED="1586542181736"/>
+</node>
+</node>
+</node>
+<node TEXT="Modo de soporte" ID="ID_1016376811" CREATED="1586541715333" MODIFIED="1586541722763">
+<node TEXT="Para pacientes que respiran hasta cierto punto" ID="ID_1101115781" CREATED="1586541723673" MODIFIED="1586541740724"/>
+<node TEXT="Establecer" ID="ID_745322495" CREATED="1586542195977" MODIFIED="1586542199208">
+<node TEXT="Corriente inspiratoria" ID="ID_780932108" CREATED="1586542199208" MODIFIED="1586542204098"/>
+<node TEXT="Corriente espiratoria" ID="ID_1529178677" CREATED="1586542204228" MODIFIED="1586542210958"/>
+</node>
+<node TEXT="Detectar cuando un paciente inspira para aplicar presion inspiratoria" ID="ID_850707675" CREATED="1586542233370" MODIFIED="1586542255830"/>
+<node TEXT="Detectar cuando un paciente exhala para aplicar corriente espiratoria" ID="ID_1275772139" CREATED="1586542256110" MODIFIED="1586542282412">
+<node TEXT="La presion espiratoria es positiva tambien pero muy baja para permitir la exhalacion" ID="ID_1002926197" CREATED="1586542288112" MODIFIED="1586542330734"/>
+</node>
+</node>
+</node>
+<node TEXT="Terminolog&#xed;a" ID="ID_292506276" CREATED="1586542801409" MODIFIED="1586543913771"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      y otros datos necesarios
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="Volumen corriente o tidal" ID="ID_329111455" CREATED="1586542496544" MODIFIED="1586542506553">
+<node TEXT="El volumen corriente o tidal (VC) es el volumen de aire que circula entre una inspiraci&#xf3;n y espiraci&#xf3;n normal sin realizar un esfuerzo adicional. El valor normal es de aproximadamente 500 ml o 7 ml/kg de peso corporal" ID="ID_792069837" CREATED="1586542506553" MODIFIED="1586542509804"/>
+<node ID="ID_1702913937" CREATED="1586542853468" MODIFIED="1586542853468"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    Vol&#250;menes y capacidades pulmonares VR: Volumen de reserva VRI: Volumen de reserva inspiratorio VRE: Volumen de reserva expiratorio CV: Capacidad vital CRF: Capacidad residual funcional CI: Capacidad inspirada CPT: Capacidad pulmonar total
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="400px-Vol&#xfa;menes_pulmonares_est&#xe1;ticos.png" ID="ID_261830998" CREATED="1586542863773" MODIFIED="1586542863778">
+<hook URI="../../../AppData/Local/Temp/400px-Vol&#xfa;menes_pulmonares_est&#xe1;ticos.png" SIZE="1.0" NAME="ExternalObject"/>
+</node>
+</node>
+</node>
+<node TEXT="Frecuencia respiratoria" ID="ID_1542841894" CREATED="1586542814099" MODIFIED="1586543927554">
+<node TEXT="de 0 a 150 respiraciones por minuto" ID="ID_38097685" CREATED="1586543927554" MODIFIED="1586543938701"/>
+</node>
+<node TEXT="Presion positiva aerea continua" ID="ID_595088218" CREATED="1586543962135" MODIFIED="1586543969742">
+<node TEXT="de 0 a 20cm H2O" ID="ID_918273450" CREATED="1586543969742" MODIFIED="1586544001025"/>
+<node TEXT="se aplica un flujo de aire moderado para generar esta presion" ID="ID_1713310975" CREATED="1586544526398" MODIFIED="1586544568452"/>
+<node TEXT="Mantiene las vias aereas abiertas continuamente en personas que pueden respirar expontaneamente por su propia cuenta pero que requieren ayuda para mantener sus vias respiratorias sin obstruirse." ID="ID_1427112383" CREATED="1586544615959" MODIFIED="1586544690067"/>
+</node>
+</node>
+</node>
+</node>
+</map>
