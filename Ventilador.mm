60d79d38fbeb577f51e4bfc190616209d15cf8e4
Sifcoon <abedul00@gmail.com>
2020-04-10 15:33:31 -0500

    Update Ventilador.mm

--- Ventilador.mm
+++ Ventilador.mm
@@ -1,6 +1,6 @@
 <map version="freeplane 1.7.0">
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
-<node TEXT="Ventilador" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1211756865" CREATED="1586541323287" MODIFIED="1586541606777"><hook NAME="MapStyle">
+<node TEXT="Ventilador" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1211756865" CREATED="1586541323287" MODIFIED="1586541606777"><hook NAME="MapStyle" zoom="0.909">
     <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
 
 <map_styles>
@@ -136,7 +136,7 @@
 </node>
 </node>
 </node>
-<node TEXT="Terminolog&#xed;a" ID="ID_292506276" CREATED="1586542801409" MODIFIED="1586543913771"><richcontent TYPE="DETAILS">
+<node TEXT="Terminolog&#xed;a" ID="ID_292506276" CREATED="1586542801409" MODIFIED="1586548919534"><richcontent TYPE="DETAILS">
 
 <html>
   <head>
@@ -144,7 +144,7 @@
   </head>
   <body>
     <p>
-      y otros datos necesarios
+      y otras especificaciones
     </p>
   </body>
 </html>
@@ -164,18 +164,171 @@
 </html>
 
 </richcontent>
-<node TEXT="400px-Vol&#xfa;menes_pulmonares_est&#xe1;ticos.png" ID="ID_261830998" CREATED="1586542863773" MODIFIED="1586542863778">
-<hook URI="../../../AppData/Local/Temp/400px-Vol&#xfa;menes_pulmonares_est&#xe1;ticos.png" SIZE="1.0" NAME="ExternalObject"/>
+<node TEXT="png_927537813598462888.png" ID="ID_1851105017" CREATED="1586547620584" MODIFIED="1586547620584">
+<hook URI="Ventilador_files/png_927537813598462888.png" SIZE="0.8759124" NAME="ExternalObject"/>
 </node>
 </node>
 </node>
 <node TEXT="Frecuencia respiratoria" ID="ID_1542841894" CREATED="1586542814099" MODIFIED="1586543927554">
-<node TEXT="de 0 a 150 respiraciones por minuto" ID="ID_38097685" CREATED="1586543927554" MODIFIED="1586543938701"/>
+<node TEXT="de 0 a 150 respiraciones por minuto" ID="ID_38097685" CREATED="1586543927554" MODIFIED="1586548905343">
+<font BOLD="true"/>
 </node>
-<node TEXT="Presion positiva aerea continua" ID="ID_595088218" CREATED="1586543962135" MODIFIED="1586543969742">
-<node TEXT="de 0 a 20cm H2O" ID="ID_918273450" CREATED="1586543969742" MODIFIED="1586544001025"/>
-<node TEXT="se aplica un flujo de aire moderado para generar esta presion" ID="ID_1713310975" CREATED="1586544526398" MODIFIED="1586544568452"/>
+</node>
+<node TEXT="Presion positiva aerea continua" ID="ID_595088218" CREATED="1586543962135" MODIFIED="1586547522861"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      CPAP
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="de 0 a 20cm H2O" ID="ID_918273450" CREATED="1586543969742" MODIFIED="1586548903746">
+<font BOLD="true"/>
+</node>
+<node TEXT="se aplica un flujo de aire moderado para generar esta presion" ID="ID_1713310975" CREATED="1586544526398" MODIFIED="1586544568452">
+<node TEXT="png_2787266800634034988.png" ID="ID_915057135" CREATED="1586547578963" MODIFIED="1586547578963">
+<hook URI="Ventilador_files/png_2787266800634034988.png" SIZE="1.0" NAME="ExternalObject"/>
+</node>
+</node>
 <node TEXT="Mantiene las vias aereas abiertas continuamente en personas que pueden respirar expontaneamente por su propia cuenta pero que requieren ayuda para mantener sus vias respiratorias sin obstruirse." ID="ID_1427112383" CREATED="1586544615959" MODIFIED="1586544690067"/>
+</node>
+<node TEXT="Presion de soporte" ID="ID_1535390354" CREATED="1586548303737" MODIFIED="1586548309023">
+<node TEXT="Es para pacientes que pueden tomar inhalaciones expontaneas" ID="ID_1759547625" CREATED="1586548309023" MODIFIED="1586548324432"/>
+<node TEXT="de 0 a 45cm de H2O" ID="ID_1129963931" CREATED="1586548886764" MODIFIED="1586548899350">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="Flujo inspiratorio" ID="ID_790192773" CREATED="1586549215715" MODIFIED="1586549220916">
+<node TEXT="de 0 a 150L/min" ID="ID_1432973219" CREATED="1586549220916" MODIFIED="1586549234903">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="Presion inspiratoria" ID="ID_945710262" CREATED="1586549243108" MODIFIED="1586549252722">
+<node TEXT="de 0 a 80cm de H2O" ID="ID_1739285526" CREATED="1586549252722" MODIFIED="1586549263808"/>
+</node>
+<node TEXT="Alarmas" ID="ID_675174688" CREATED="1586549908226" MODIFIED="1586549911201">
+<node TEXT="Apnea" ID="ID_510877016" CREATED="1586549911201" MODIFIED="1586550110683">
+<font BOLD="true"/>
+<node TEXT="cese completo de la se&#xf1;al respiratoria por mas de 15 s" ID="ID_868602479" CREATED="1586549921421" MODIFIED="1586549950058"/>
+</node>
+<node TEXT="Volumen minuto o corriente alto o bajo" ID="ID_1247764600" CREATED="1586549960287" MODIFIED="1586550111182">
+<font BOLD="true"/>
+</node>
+<node TEXT="Presion inspiratoria alta" ID="ID_1546059244" CREATED="1586549971608" MODIFIED="1586550111683">
+<font BOLD="true"/>
+</node>
+<node TEXT="Frecuencia respiratoria alta" ID="ID_899805176" CREATED="1586549987868" MODIFIED="1586550112182">
+<font BOLD="true"/>
+</node>
+<node TEXT="FiO2 alta y baja" ID="ID_1073930669" CREATED="1586550014558" MODIFIED="1586550112642">
+<font BOLD="true"/>
+<node TEXT="Medir proporcion de oxigeno  (medidor de oxigeno)" ID="ID_788376893" CREATED="1586550121293" MODIFIED="1586550136564"/>
+</node>
+<node TEXT="baja presion del suministro de gases" ID="ID_672621740" CREATED="1586550029868" MODIFIED="1586550113093">
+<font BOLD="true"/>
+</node>
+<node TEXT="Bateria baja" ID="ID_1173306379" CREATED="1586550037129" MODIFIED="1586550113523">
+<font BOLD="true"/>
+</node>
+<node TEXT="Falta de alimentacion electrica" ID="ID_278321261" CREATED="1586550043299" MODIFIED="1586550113983">
+<font BOLD="true"/>
+</node>
+<node TEXT="Ventilador inoperable" ID="ID_1810864335" CREATED="1586550056410" MODIFIED="1586550114451">
+<font BOLD="true"/>
+</node>
+<node TEXT="Silencio de alarma" ID="ID_699993137" CREATED="1586550096322" MODIFIED="1586550116663">
+<font BOLD="true"/>
+<node TEXT="??" ID="ID_302393217" CREATED="1586550100512" MODIFIED="1586550104652"/>
+</node>
+<node TEXT="Ventilador inoperante o falla del ventilador" ID="ID_644592558" CREATED="1586550062160" MODIFIED="1586550117153">
+<font BOLD="true"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Sistemas necesarios" POSITION="right" ID="ID_513950552" CREATED="1586547228725" MODIFIED="1586547240141">
+<node TEXT="Ciclos de respiracion" ID="ID_20383877" CREATED="1586547305357" MODIFIED="1586547317133">
+<node TEXT="Motorreductor CD" ID="ID_1098925286" CREATED="1586547317136" MODIFIED="1586547338486"/>
+<node TEXT="Arduino" ID="ID_588798168" CREATED="1586547338630" MODIFIED="1586547348198"/>
+<node TEXT="Potencia" ID="ID_675000914" CREATED="1586547348485" MODIFIED="1586547351470">
+<node TEXT="LD298" ID="ID_1007679680" CREATED="1586547353382" MODIFIED="1586547358102"/>
+</node>
+<node TEXT="Bolsa valvula mascarilla (Ambu)" ID="ID_1810486703" CREATED="1586547240143" MODIFIED="1586547299134"/>
+</node>
+<node TEXT="Presion positiva Aerea continua" ID="ID_336245567" CREATED="1586547360621" MODIFIED="1586547372150">
+<node TEXT="Alternativas" ID="ID_1434703920" CREATED="1586547820809" MODIFIED="1586547824774">
+<node TEXT="Bomba de aire que genere flujo a cierta presi&#xf3;n" ID="ID_624210070" CREATED="1586547372152" MODIFIED="1586547399999"/>
+<node ID="ID_1626061983" CREATED="1586547736528" MODIFIED="1586547803485"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Entrada de aire de compresor o tanque de O<sub>2&#160;</sub>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="2 rotametros" ID="ID_1449219806" CREATED="1586547879172" MODIFIED="1586547886602">
+<node TEXT="1 para aire" ID="ID_310896557" CREATED="1586547886602" MODIFIED="1586547890443"/>
+<node ID="ID_296778062" CREATED="1586547890628" MODIFIED="1586547914008"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      1 para O<sub>2&#160;</sub>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Medidor de proporcion de gases" ID="ID_1481620453" CREATED="1586550632230" MODIFIED="1586550643490">
+<node ID="ID_140444329" CREATED="1586550687372" MODIFIED="1586550709653"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Sensor de O<sub>2</sub>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="https://articulo.mercadolibre.com.mx/MLM-555393446-sensor-de-gas-o2-oxigeno-arduino-avr-pic-_JM#position=2&amp;type=item&amp;tracking_id=e786dc63-a0c0-4d05-8259-c85687f25fc8" ID="ID_395403551" CREATED="1586550643500" MODIFIED="1586550679972" LINK="https://articulo.mercadolibre.com.mx/MLM-555393446-sensor-de-gas-o2-oxigeno-arduino-avr-pic-_JM#position=2&amp;type=item&amp;tracking_id=e786dc63-a0c0-4d05-8259-c85687f25fc8"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      $3,348.00
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
 </node>
 </node>
 </node>
